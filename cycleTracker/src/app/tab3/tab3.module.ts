4dd779bbd7136ef32fcc33c788f78ee5cf536379
PaulinaNiederhoefer <paulina.niederhoefer@mni.thm.de>
2021-05-14 12:15:42 +0200

    chart.js installiert

--- cycleTracker/src/app/tab3/tab3.module.ts
+++ cycleTracker/src/app/tab3/tab3.module.ts
@@ -5,8 +5,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+import { Chart } from "chart.js";
 import { Tab3PageRoutingModule } from './tab3-routing.module';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   imports: [
--- cycleTracker/src/app/tab3/tab3.page.ts
+++ cycleTracker/src/app/tab3/tab3.page.ts
@@ -1,18 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit} from '@angular/core';
 import {PopoverController} from '@ionic/angular';
 import {PopoverViewerComponent} from '../popover-viewer/popover-viewer.component';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit{
+  @ViewChild('lineCanvas') lineCanvas;
+  lineChart: any;
   /**
    * Popover initial in Tab3
    */
   constructor(private popoverController: PopoverController) { }
 
+  ngOnInit() {
+    this.lineChartMethod();
+  }
+
+  lineChartMethod() {
+
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December'],
+        datasets: [
+          {
+            label: 'Sell per week',
+            fill: false,
+            //lineTension: 0.5,
+            backgroundColor: 'rgba(75,220,150,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40, 10, 5, 50, 10, 15],
+            spanGaps: false,
+          }
+        ]
+      }
+    });
+  }
   async viewPopover() {
     const popover = await this.popoverController.create({
       component: PopoverViewerComponent,
@@ -29,9 +69,5 @@ export class Tab3Page {
     /** Sync event from popover component */
 
   }
-  /**
-   * Popover initial in Tab3 end
-   */
-
 
 }
