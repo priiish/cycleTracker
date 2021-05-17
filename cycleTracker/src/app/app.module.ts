1aab3d5e3cf42c72a6e01ef5106c346b27c76b06
Lennart Uhrmacher <lennartuhrmacher@gmail.com>
2021-05-18 00:46:32 +0200

    Removed SQLite module

--- cycleTracker/src/app/app.module.ts
+++ cycleTracker/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SQLite } from "@ionic-native/sqlite/ngx";
 import { IonicStorageModule } from '@ionic/storage-angular';
 
 
@@ -12,7 +11,7 @@ import { IonicStorageModule } from '@ionic/storage-angular';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, SQLite],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
