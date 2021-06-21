e7c57d0595a0d5376ca3f9891569c0008bf6cfa5
Korikovd <korikovda96@gmail.com>
2021-06-21 10:49:57 +0200

    Add dark-mode function and rebuild a pop-over

--- cycleTracker/src/app/popover-viewer/popover-viewer.component.ts
+++ cycleTracker/src/app/popover-viewer/popover-viewer.component.ts
@@ -18,9 +18,21 @@ export class PopoverViewerComponent implements OnInit {
   }
   // eslint-disable-next-line @typescript-eslint/naming-convention
   ngOnInit(): void {
+    if(document.body.className == 'dark backdrop-no-scroll'){
+      document.getElementById('dark__toggle').classList.add('toggle-checked');
+    }
   }
   /**
    * Popover Items funktions end
    */
-
+  /* Toggle-Button-Event */
+  /**
+   * No exception, problem with compiling ts and js
+    * @param event
+   */
+  onClick(event){
+    document.body.classList.toggle('dark');
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    document.getElementById("calendar__edit").classList.toggle('calender--black');
+  }
 }
